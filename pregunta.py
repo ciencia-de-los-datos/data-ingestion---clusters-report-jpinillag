8f829f5be7e1070341b8abb2fb7f7e91c2440908
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-04-03 20:17:48 -0500

    Resolucion
    Resolucion inicial a la pregunta

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,19 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf("clusters_report.txt",skiprows = 4,names = ["cluster", "cantidad _de_palabras_clave","porcentaje_de_palabras_clave", "principales_palabras_clave"])
+    df.fillna(0, inplace=True)
+    i=1
+    while i < len(df["cluster"]):
+        if(df["cluster"].iloc[i]<df["cluster"].iloc[i-1]):
+            df["cluster"].iloc[i]=df["cluster"].iloc[i-1]
+        if(df["cantidad _de_palabras_clave"].iloc[i]==0):
+            df["cantidad _de_palabras_clave"].iloc[i]=df["cantidad _de_palabras_clave"].iloc[i-1]
+        if(df["porcentaje_de_palabras_clave"].iloc[i]==0):
+            #print("es 0")
+            df["porcentaje_de_palabras_clave"].iloc[i]=df["porcentaje_de_palabras_clave"].iloc[i-1]
+        i+=1
+    
+    df = df.groupby(["cluster","cantidad _de_palabras_clave","porcentaje_de_palabras_clave"])["principales_palabras_clave"].apply(' '.join).reset_index()
 
     return df
